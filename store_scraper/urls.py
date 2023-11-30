5803151a04ac2fd2d0a785b46be79817e9149d16
kokann <kokann_92@hotmail.com>
2023-11-30 21:40:55 +0800

    Added a simple view for store scraper index

--- store_scraper/urls.py
+++ store_scraper/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "store_scraper"
 
 urlpatterns = [
-
+    path("", views.index, name="index"),
 ]
--- store_scraper/views.py
+++ store_scraper/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {"title": "Store Scraper"}
+    return render(request, "store_scraper/index.html", context)
