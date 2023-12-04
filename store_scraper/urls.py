70316a80c9fdf036f0fd747d25aea236af4bb369
kokann <kokann_92@hotmail.com>
2023-12-04 21:32:04 +0800

    Update urls

--- store_scraper/urls.py
+++ store_scraper/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "store_scraper"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("scrape/", views.index, name="index"),
+	path("api/coffee-stores/", views.CoffeeStoreViewSet.as_view({"get": "list"}), name="coffee_stores_api"),
 ]
